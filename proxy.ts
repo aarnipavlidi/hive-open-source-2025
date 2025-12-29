12c0f6b1f5f354a6a486de7fdf012287057e91c0
Aarni Pavlidi <aarnidev@proton.me>
2025-12-30 00:22:32 +0200

    Refactor proxy auth to support client ID/secret headers

--- proxy.ts
+++ proxy.ts
@@ -15,43 +15,38 @@ export async function proxy(request: NextRequest) {
   // TODO: Make this more secure?
   if (ENV.NODE_ENV === 'development') {
     return NextResponse.next();
+  };
+
+  const cloudflareAccessClientID = request.headers.get('cf-access-client-id');
+  console.log('cloudflareAccessClientID', cloudflareAccessClientID);
+  const cloudflareAccessClientSecret = request.headers.get('cf-access-client-secret');
+  console.log('cloudflareAccessClientSecret', cloudflareAccessClientSecret);
+
+  if (cloudflareAccessClientID === ENV.CF_ACCESS_CLIENT_ID && cloudflareAccessClientSecret === ENV.CF_ACCESS_CLIENT_SECRET) {
+    return NextResponse.next();
   }
 
   const cloudflareJWT = request.headers.get('cf-access-jwt-assertion');
+  console.log('cloudflareJWT', cloudflareJWT);
 
-  if (!cloudflareJWT) {
-    const errorResponse = {
-      error: 'You are not authorized to access this resource, as you are missing token. Please try again later.'
-    };
+  if (cloudflareJWT) {
+    try {
+      const JWKS = jose.createRemoteJWKSet(new URL(CLOUDFLARE_TEAM_CERTS));
+      await jose.jwtVerify(cloudflareJWT, JWKS, {
+        issuer: ENV.CF_TEAM_DOMAIN,
+        audience: CLOUDFLARE_AUDIENCE_TAG
+      });
+      return NextResponse.next();
+    } catch (err) {
+      console.error("JWT Verify Failed:", err);
+    }
+  }
 
-    return new NextResponse(JSON.stringify(errorResponse), {
-      status: 401,
-      headers: {
-        'content-type': 'application/json'
-      },
-    });
-  };
-
-  try {
-    const JWKS = jose.createRemoteJWKSet(new URL(CLOUDFLARE_TEAM_CERTS));
-    await jose.jwtVerify(cloudflareJWT, JWKS, {
-      issuer: CLOUDLARE_TEAM_DOMAIN,
-      audience: CLOUDFLARE_AUDIENCE_TAG,
-    });
-
-    return NextResponse.next();
-  } catch (err) {
-    const errorResponse = {
-      error: 'You are not authorized to access this resource, as your token may be invalid or expired. Please try again later.'
-    };
-
-    return new NextResponse(JSON.stringify(errorResponse), {
-      status: 403,
-      headers: { 'content-type': 'application/json' },
-    });
-  };
-};
-
+  return new NextResponse(JSON.stringify({ error: 'Unauthorized Access' }), {
+    status: 401,
+    headers: { 'content-type': 'application/json' },
+  });
+}
 
 export const config = {
   matcher: ['/api/:path*'],
